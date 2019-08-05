5e97e9f72d0d3bc7d642588fc9e28489dfa5399a
chen <chenzhichai210@126.com>
2019-08-05 11:09:26 +0800

    registerActivityLifecycleCallbacks方法控制退出的时候背景音乐停止。

--- app/src/main/java/com/chai/colin/MyApp.java
+++ app/src/main/java/com/chai/colin/MyApp.java
@@ -1,18 +1,27 @@
 package com.chai.colin;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.chai.colin.util.MediaPlayUtil;
 import com.chai.colin.util.SPUtils;
 import com.chai.colin.util.SoundPoolUtil;
 import com.chai.colin.util.ToastUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyApp extends Application {
     private static final String TAG = "MyAppcation";
     private static MyApp mInstance;
     boolean accountBalance;
-//    private List<Activity> activities = new ArrayList();
+    private List<Activity> activities = new ArrayList<>();
     private int appCount;
     double balance;
     private long exitTime = 0L;
@@ -27,11 +36,70 @@ public class MyApp extends Application {
 
         SoundPoolUtil.getInstance(this);
         MediaPlayUtil.getInstance(this);
-
         SPUtils.getInstance().init(this);
         //在这里为应用设置异常处理程序，然后我们的程序才能捕获未处理的异常
         CrashHandler crashHandler = CrashHandler.getInstance();
         crashHandler.init(this);
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
+//                if ((activity instanceof WebviewGameActivity)) {
+//                    MediaPlayUtil.resume();
+//                }
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+                appCount++;
+                Log.e("start", String.valueOf(appCount));
+                if (isRunInBackground) {
+                    back2App(activity);
+                }
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+//                if ((activity instanceof WebviewGameActivity)) {
+//                    MediaPlayUtil.pause();
+//                }
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+                appCount--;
+                Log.e("stop", String.valueOf(appCount));
+                if (appCount == 0) {
+                    leaveApp(activity);
+                }
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
+    }
+
+    private void leaveApp(Activity activity) {
+        isRunInBackground = true;
+        MediaPlayUtil.pause();
+    }
+
+    private void back2App(Activity activity) {
+        isRunInBackground = false;
+        MediaPlayUtil.resume();
     }
 
     public static Context getContext() {
